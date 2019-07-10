936e1b07844865e83106ce9615dd770854c7b09d
Kelsey Low <kklowlow@gmail.com>
2019-07-10 02:20:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'application#hello'
 
   get '/secret', to: 'secrets#show'
-  
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
